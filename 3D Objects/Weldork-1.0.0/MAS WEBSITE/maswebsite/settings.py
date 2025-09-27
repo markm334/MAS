366ab61b0e6c89aab643de629b3d73bd517fab61
Mark Murangiri <markmurangiri885@gmail.com>
2025-09-27 20:49:19 +0300

    Updated website with video section

--- 3D Objects/Weldork-1.0.0/MAS WEBSITE/maswebsite/settings.py
+++ 3D Objects/Weldork-1.0.0/MAS WEBSITE/maswebsite/settings.py
@@ -158,7 +158,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ndiatvcict@gmail.com'
-EMAIL_HOST_PASSWORD = 'gsmc marj owbm ppcc'
+EMAIL_HOST_PASSWORD = 'xgkimckiukidapzh'
 DEFAULT_FROM_EMAIL = 'ndiatvcict@gmail.com'
 
 
