b05cb70e00c7b87e8a7c7c07dc0692e78dde4cb3
Mark Murangiri <markmurangiri885@gmail.com>
2025-10-17 17:18:22 +0300

    chore: commit all .py files after template integration

---
+++ epmiti/core/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+	return render(request, 'index.html')
+
+
+def about(request):
+	return render(request, 'about.html')
+
+
+def contact(request):
+	return render(request, 'contact.html')
+
+
+def blog(request):
+	return render(request, 'blog.html')
+
+
+def gallery(request):
+	return render(request, 'gallery.html')
+
+
+def events(request):
+	return render(request, 'events.html')
+
+
+def donation(request):
+	return render(request, 'donation.html')
+
+
+def volunteer(request):
+	return render(request, 'volunteer.html')
---
+++ epmiti/epmiti/urls.py
@@ -0,0 +1,31 @@
+"""
+URL configuration for epmiti project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from core import views as core_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', core_views.index, name='index'),
+    path('about/', core_views.about, name='about'),
+    path('contact/', core_views.contact, name='contact'),
+    path('blog/', core_views.blog, name='blog'),
+    path('gallery/', core_views.gallery, name='gallery'),
+    path('events/', core_views.events, name='events'),
+    path('donation/', core_views.donation, name='donation'),
+    path('volunteer/', core_views.volunteer, name='volunteer'),
+]
